27802b0539036c43648267999a7c9a0fcfc079ef
GetStuffDone <t.targetytt@gmail.com>
2020-05-05 06:58:38 +0000

    updated main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def HOME_PAGE():
-	return render_template("templates/home.html")
+	return render_template("/templates/home.html")
 
 if __name__=='__main__':
 	app.run(debug=True)
