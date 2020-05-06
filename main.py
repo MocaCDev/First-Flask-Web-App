0268770db1ac9bba86e7fe45ce6de1793767aa21
GetStuffDone <t.targetytt@gmail.com>
2020-05-06 19:58:18 +0000

    updated main.py

--- main.py
+++ main.py
@@ -29,9 +29,9 @@ def HOME_PAGE():
 		usernames_.append(request.form['Username'])
 		for i in filter_out:
 			for x in range(len(usernames_)):
-				if i in usernames_[x]:
+				if i in usernames_[x].toLower():
 					del usernames_[x]
-					return '<h1>Username is bad.</h1> <button type="submit"><a href="/" style="color:white;text-decoration:none">BACK</a></button>'
+					return '<h1>Username is bad.</h1> <button type="submit"><a href="/" style="color:white;text-decoration:none;background-color:black">BACK</a></button>'
 		return render_template('submit.html', username=usernames_)
 	else:
 		return render_template("userSetup.html")
