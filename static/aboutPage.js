0dc8707137673776d24a81315d2947fc93495416
GetStuffDone <t.targetytt@gmail.com>
2020-05-06 04:00:33 +0000

    updated aboutPage.js

--- static/aboutPage.js
+++ static/aboutPage.js
@@ -1,7 +1,7 @@
-window.onload = () => {
+window.addEventListener('load', () => {
 	let WelcomeHeader = document.getElementById('WelcomeHeader');
 	// Making "About Page" for header of about.html
 	let NewElement = document.createElement('h1');
 	NewElement.innerText = "About Page";
 	WelcomeHeader.appendChild(NewElement);
-}
+});
