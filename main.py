81e4ff3e0c71d0850094c04b45118af08ca664dc
GetStuffDone <t.targetytt@gmail.com>
2020-05-08 19:14:24 +0000

    updated main.py

--- main.py
+++ main.py
@@ -11,13 +11,16 @@ try:
 	)
 except ImportError as IE:
 	print("IE ERR: %s" % IE)
-	os.system('pip3 install -r requirements.txt')
-	from flask import (
-		Flask,
-		render_template,
-		#url_for,
-		request
-	)
+	try:
+		os.system('pip3 install -r requirements.txt')
+		from flask import (
+			Flask,
+			render_template,
+			#url_for,
+			request
+		)
+	except:
+		raise ImportError('pip3 is not installed. pip3 is needed in order to download imported module "Flask"')
 
 app = Flask(__name__)
 # NAMES TO FILTER OUT
