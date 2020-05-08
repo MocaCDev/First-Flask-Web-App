18292d07cfe2f0e281cd5e330fe0f423ecb3d286
GetStuffDone <t.targetytt@gmail.com>
2020-05-08 04:59:53 +0000

    updated main.py

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def HOME_PAGE():
 			for i in filter_out:
 				for x in range(len(usernames_)):
 					if i in usernames_[x].lower():
-						for l in len(usernames_[x].lower()):
+						for l in range(len(usernames_[x])):
 							if usernames_[x][l].lower() == i[0] and usernames_[x][l-1].lower() == '':
 								message='<h3>Username %s is bad.</h3><br><p>Contains the word: %s</p><a href="/" style="text-decoration:none"><button type="submit" style="background-color:black;color:white">Go Back</button></a>'%(usernames_[x],i)
 								del usernames_[x]
